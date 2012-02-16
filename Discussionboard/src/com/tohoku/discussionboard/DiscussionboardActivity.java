451bf2a9b2b3e720a175d72bba7a00df46da94ed
Sergii Pechenizkyi <plastivreg@gmail.com>
2012-02-16 12:20:21 +0200

    Create new prj

---
+++ Discussionboard/src/com/tohoku/discussionboard/DiscussionboardActivity.java
@@ -0,0 +1,15 @@
+package com.tohoku.discussionboard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DiscussionboardActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
