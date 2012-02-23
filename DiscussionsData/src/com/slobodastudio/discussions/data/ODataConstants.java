9020a791f4a8ac5073191b3fbea01d33ca812d6a
Sergii Pechenizkyi <plastivreg@gmail.com>
2012-02-23 09:03:37 +0200

    Added odata lib and simple read write operation

---
+++ DiscussionsData/src/com/slobodastudio/discussions/data/ODataConstants.java
@@ -0,0 +1,8 @@
+package com.slobodastudio.discussions.data;
+
+/** Helper class that consist of constants for open data protocol client SDK. */
+public class ODataConstants {
+
+	/** Link to Japan server with database layer service */
+	public static final String DISCUSSIONS_JAPAN = "http://123.108.5.30/DiscSvc/discsvc.svc/";
+}
