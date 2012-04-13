0e8d4615484bfd22466179e901ec7c3deb146188
Sergii Pechenizkyi <plastivreg@gmail.com>
2012-04-13 18:01:09 +0300

    added new layouts, updated robotium

---
+++ Discussions/src/com/slobodastudio/discussions/photon/constants/DeviceType.java
@@ -0,0 +1,14 @@
+package com.slobodastudio.discussions.photon.constants;
+
+public class DeviceType {
+
+	public static final int ANDROID = 0;
+	public static final int STICKY = 2;
+	public static final int WPF = 1;
+
+	/** A private Constructor prevents class from instantiating. */
+	private DeviceType() {
+
+		throw new UnsupportedOperationException("Class is prevented from instantiation");
+	}
+}
---
+++ Discussions/src/com/slobodastudio/discussions/ui/IntentAction.java
@@ -0,0 +1,12 @@
+package com.slobodastudio.discussions.ui;
+
+public class IntentAction {
+
+	public static final String NEW = "slobodastudio.intent.action.NEW";
+
+	/** A private Constructor prevents class from instantiating. */
+	private IntentAction() {
+
+		throw new UnsupportedOperationException("Class is prevented from instantiation");
+	}
+}
